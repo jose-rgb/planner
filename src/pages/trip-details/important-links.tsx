0c00e7c80ae2fb94570894367cf2f4588c8360a5
José Ricardo <josericardosaraiva7@gmail.com>
2024-07-11 21:23:28 -0300

    feat: fetch importants links

--- src/pages/trip-details/important-links.tsx
+++ src/pages/trip-details/important-links.tsx
@@ -1,37 +1,47 @@
+import { useEffect, useState } from "react";
 import { Link2, Plus } from "lucide-react";
 import { Button } from "../../components/button";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface ImportantLinksProps {
   openCreateImportantLinkModal: () => void
 }
 
+interface Link {
+  title: string;
+  url: string;
+}
+
+
 export function ImportantLinks({openCreateImportantLinkModal}: ImportantLinksProps) {
+  const { tripId } = useParams()
+  const [links, setLinks] = useState<Link[]>([])
+
+  useEffect(() => {
+    api.get(`trip/${tripId}/links`).then(response => setLinks(response.data.links))
+  }, [tripId])
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Links importantes</h2>
 
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-            </a>
-          </div>
 
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+        {links.map(link => {
+          return (
+            <div key={link.title} className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="block font-medium text-zinc-100">{link.title}</span>
+                <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
+                  {link.url}
+                </a>
+              </div>
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
-            <a href="#" className="block text-xs text-zinc-400 truncate hover:text-zinc-200">
-              https://www.airbnb.com.br/rooms/10470001139028321098312093821903812038910
-            </a>
-          </div>
-
-          <Link2 className="text-zinc-400 size-5 shrink-0" />
-        </div>
+              <Link2 className="text-zinc-400 size-5 shrink-0" />
+            </div>
+          )
+        })}
       </div>
 
       <Button onClick={openCreateImportantLinkModal} variant="secondary" size="full">
